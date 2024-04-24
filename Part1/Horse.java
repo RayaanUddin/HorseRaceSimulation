dcbbd9e63af14d38a4f68ba66caa3f2e813fe5c4
Rayaan Uddin <rayaan.uddin@outlook.com>
2024-04-24 19:15:02 +0100

    Horse object requirements set. Accessor methods, required attributes, and dynamic confidence level on history of horse races

--- Part1/Horse.java
+++ Part1/Horse.java
@@ -1,14 +1,18 @@
 
 /**
- * Write a description of class Horse here.
+ * Abstract version of a horse within the race. Represents horse using its name, distance, if fallen, confidence and holds it symbol to be used.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Rayaan Uddin
+ * @version 1.0
  */
 public class Horse
 {
     //Fields of class Horse
-    
+    private final String horseName;
+    private char horseSymbol;
+    private double horseConfidence;
+    private int horseDistance;
+    private boolean horseFallen;
     
       
     //Constructor of class Horse
@@ -17,60 +21,74 @@ public class Horse
      */
     public Horse(char horseSymbol, String horseName, double horseConfidence)
     {
-       
+       this.horseName = horseName;
+       this.horseSymbol = horseSymbol;
+       this.horseConfidence = horseConfidence;
     }
-    
-    
-    
+
     //Other methods of class Horse
     public void fall()
     {
-        
+        horseFallen = true;
+        horseConfidence = horseConfidence * 0.9; // Confidence level drops by 10% when horse falls
     }
+
+    public void goBackToStart()
+    {
+        horseDistance = 0;
+        horseFallen = false;
+    }
+
+    // If horse wins, increase confidence by 10%
+    public void win()
+    {
+        horseConfidence = horseConfidence * 1.1;
+    }
+
+    // Accessor methods
     
     public double getConfidence()
     {
-        
+        return horseConfidence;
     }
     
     public int getDistanceTravelled()
     {
-        
+        return horseDistance;
     }
     
     public String getName()
     {
-        
+        return horseName;
     }
     
     public char getSymbol()
     {
-        
-    }
-    
-    public void goBackToStart()
-    {
-        
+        return horseSymbol;
     }
     
     public boolean hasFallen()
     {
-        
+        return horseFallen;
     }
 
     public void moveForward()
     {
-        
+        horseDistance++;
     }
 
     public void setConfidence(double newConfidence)
     {
-        
+        if (newConfidence<=1 && newConfidence>=0) {
+            horseConfidence = newConfidence;
+        } else {
+            System.out.println("Confidence Level must be between 0 and 1");
+        }
     }
     
     public void setSymbol(char newSymbol)
     {
-        
+        horseSymbol = newSymbol;
     }
     
 }
